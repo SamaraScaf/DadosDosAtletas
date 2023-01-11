840290e6812813ffb499e67c222da934f66f7cce
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-01-10 23:09:46 -0300

    Add files via upload

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,43 +1,48 @@
 class Atleta {
-    constructor(nome, idade, peso, altura, notas) {
-        this.nome = nome;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
-        this.notas = notas
+    contructor (nome,idade,peso,altura,notas){
+    this.nome=nome;
+    this.idade=idade;
+    this.peso=peso;
+    this.altura=altura;
+    this.notas=notas
     }
-
     calculaCategoria() {
-        if (this.idade > 8 && this.idade < 12)
-            return "Infantil"
-        if (this.idade > 11 && this.idade < 14)
-            return "Juvenil"
-        if (this.idade > 13 && this.idade < 16)
-            return "Intermediário"
-        if (this.idade > 15 && this.idade < 31)
-            return "Adulto"
+        if(this.idade>9&& this.idade<11){
+        return "Infantil"
+        }
+        else if(this.idade>12&& this.idade<13){
+        return "Juvenil"
+        }
+        else if(this.idade>14&& this.idade<15){
+        return "Intermediário"
+        }
+        else if(this.idade>16&& this.idade>30){
+        return "Adulto"
+        }else{
+        return "Sem categoria"
+        }
+    
+    calculaIMC(){
+    return this.peso/(this.altura*this.altura)
     }
-
-    calcularIMC() {
-        return this.peso / (this.altura * this.altura)
+    
+    calculaMediaValida(){
+    let notasTotais = this.notas;
+    let notasOrganizadas = notasTotais.sort(function comparaNumeros(a,b){
+    if (a===b){
+    return 0;
+    if (a<b){
+    return -1;
     }
-
-    calculaMediaValida() {
-        let notasGerais = this.notas;
-        let notasOrganizadas = notasGerais.sort(function comparaNumeros(a, b) {
-            if (a == b) return 0;
-            if (a < b) return -1;
-            if (a > b) return 1;
-        });
-
-
-        let notasComputadas = notasOrganizadas.slice(1, 4)
-
-        let somaNotasComputadas = notasComputadas.reduce(function (total, atual) {
-            return total + atual
-        }, 0)
-
-        let media = (somaNotasComputadas / notasComputadas.length).toFixed(2);
-
-        return media;
+    if (a>b);
+    return 1;
+    });
+    }
+    let notasComputadas = notasOrganizadas.slice(1,4)
+    let somaNotasComputadas = notasComputadas.reduce(function(total,atual){
+    return total + atual
+    },0)
+    }
+    let media = (somaNotasComputadas / notasComputadas.length)
+    return media;
     }
